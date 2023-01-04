4fa9000ed7514082864819a9bd704c7f5726ed7b
Ace <106693166+rickyavila29@users.noreply.github.com>
2023-01-03 23:32:07 -0700

    added note parameter to read me

--- note-taker/Develop/db/db.js
+++ note-taker/Develop/db/db.js
@@ -5,7 +5,7 @@ const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
 class Store {
-    read() {
+    read(note) {
       return readFileAsync('db/db.json', 'utf8');
     };
   
